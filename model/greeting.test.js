b3ad4bc57045d2d0090b09abc3b750ab1afd64d4
Matt Warman <warmanmr@gmail.com>
2017-11-03 13:54:24 -0400

    Greeting.save and .findAll tests

--- model/greeting.test.js
+++ model/greeting.test.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const expect = require('expect');
+const rewire = require('rewire');
+const sinon = require('sinon');
 
-const {Greeting} = require('./greeting');
+var {Greeting} = rewire('./greeting');
 
 describe('Greeting', () => {
 
@@ -62,6 +64,77 @@ describe('Greeting', () => {
       let obj = greeting.toObject();
 
       expect(obj).toEqual(greetingObj);
+    });
+
+  });
+
+  describe('#save', () => {
+
+    // Mock db
+    var db = {
+      put: sinon.spy()
+    };
+    // Inject Mock db into greeting module
+    var greetingModule = rewire('./greeting');
+    greetingModule.__set__('db', db);
+
+    it('should call put', () => {
+      let greeting = new greetingModule.Greeting({
+        value,
+        author
+      });
+
+      greeting.save();
+
+      expect(db.put.calledOnce).toBeTruthy();
+    });
+
+    it('should call put with param values', () => {
+      let greeting = new greetingModule.Greeting({
+        value,
+        author
+      });
+
+      greeting.save();
+
+      expect(typeof db.put.args[0]).toBe('object');
+
+      var putParams = db.put.args[0][0];
+      expect(putParams.Item.id).not.toBeNull();
+      expect(typeof putParams.Item.id).toBe('string');
+      expect(putParams.Item.value).toBe(value);
+      expect(putParams.Item.author).toBe(author);
+    });
+
+  });
+
+  describe('#findAll', () => {
+
+    // Mock db
+    var db = {
+      scan: sinon.spy()
+    };
+    var tableName = 'Greetings';
+    // Inject Mock db into greeting module
+    var greetingModule = rewire('./greeting');
+    greetingModule.__set__({
+      db,
+      tableName
+    });
+
+    it('should call scan', () => {
+      greetingModule.Greeting.findAll();
+
+      expect(db.scan.calledOnce).toBeTruthy();
+    });
+
+    it('should call scan with param values', () => {
+      greetingModule.Greeting.findAll();
+
+      expect(typeof db.scan.args[0]).toBe('object');
+
+      var scanParams = db.scan.args[0][0];
+      expect(scanParams.TableName).toBe(tableName);
     });
 
   });
